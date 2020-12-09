027f1024941fc6dea6234638d12a4e84592fbe23
dominic <dominic@rubyisabetterway.com>
2020-12-08 18:03:16 -0600

    First commit

---
+++ WebApi/Models/DCandidate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models
+{
+    public class DCandidate
+    {
+            [Key]
+            public int ID { get; set; }
+
+            [Column(TypeName="nvarchar(100)")]
+            public string fullName { get; set; }
+
+            
+            [Column(TypeName="nvarchar(16)")]
+            public string mobile {  get; set; } 
+
+
+            [Column(TypeName="nvarchar(100)")]
+            public string mail { get; set; }
+
+
+            public int age { get; set; }
+
+
+            [Column(TypeName="nvarchar(3)")]
+            public string bloodGroup { get; set; }
+            
+
+            [Column(TypeName="nvarchar(100)")]
+            public string address { get; set; }
+        
+    
+        
+    }
+}
---
+++ WebApi/Models/DonationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class DonationDBContext: DbContext
+    {
+public DonationDBContext(DbContextOptions<DonationDBContext> options):base(options)
+{
+    
+}
+public DbSet<DCandidate>  DCandidates { get; set; }
+    }
+}
